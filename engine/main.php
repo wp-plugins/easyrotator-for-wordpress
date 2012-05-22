097e0559e07de696cb3fe79d654ec442b8ce9a5b
DWUser.com <DWUser.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-22 14:15:57 +0000

    Fixing trunk for Version 1.0.2 of EasyRotator for WordPress - The engine/main.php file committed yesterday isn't appearing properly.
    git-svn-id: https://plugins.svn.wordpress.org/easyrotator-for-wordpress/tags/1.0.2@547463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/main.php
+++ engine/main.php
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
+ 
 
 // Load config class
 require_once(dirname(__FILE__) . '/includes/erconfig.php');
